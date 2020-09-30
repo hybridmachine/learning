c82eac94ada9d6b8780587451c44b9720bbfabee
Brian Tabone <btabone@luminexcorp.com>
2020-09-30 16:23:28 -0500

    Updated layering

--- matlab/udemy/CodingChallengeMatixMultLayering.m
+++ matlab/udemy/CodingChallengeMatixMultLayering.m
@@ -5,11 +5,11 @@
 M1 = randn(2,2);
 M2 = randn(2,2);
 
-for leftCol = 1:size(M1)(2)
-  for leftRow = 1:size(M1)(1)
-    for rightRow = 1:size(M2)(1)
+for layer = 1:size(M2)(1)
+  for leftCol = 1:size(M1)(2)
+    for leftRow = 1:size(M1)(1)      
       for rightCol = 1:size(M2)(2)
-        msg = sprintf("left row %d : right col %d",leftRow,rightCol);
+        msg = sprintf("left row,col %d,%d : layer,col %d,%d", leftRow,leftCol,layer,rightCol);
         disp(msg);
       endfor
     endfor
