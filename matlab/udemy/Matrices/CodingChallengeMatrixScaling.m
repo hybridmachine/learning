cd21f0408a9695f4b2c98f03fd111970d91bbe79
Brian Tabone <brian.tabone@gmail.com>
2020-10-16 12:16:42 -0500

    Integrated code from lesson (basically same as my code) then added animation

--- matlab/udemy/Matrices/CodingChallengeMatrixScaling.m
+++ matlab/udemy/Matrices/CodingChallengeMatrixScaling.m
@@ -1,4 +1,5 @@
 % Generate XY coordinates for circle
+r = 1;
 th = 0:pi/100:2*pi;
 xunit = r * cos(th);
 yunit = r * sin(th);
@@ -8,7 +9,26 @@ scalingMatrix=[1 0; 0 3];
 coordMatrix = [ xunit; yunit];
 scaledCoords = scalingMatrix * coordMatrix;  
 
-figure(1),clf;
-hold on
-plot(coordMatrix(1,:),coordMatrix(2,:))
-plot(scaledCoords(1,:),scaledCoords(2,:))
+
+
+% plot(coordMatrix(1,:),coordMatrix(2,:))
+% plot(scaledCoords(1,:),scaledCoords(2,:))
+
+% From the lecture
+
+x = linspace(-pi, pi, 100);
+xy = [cos(x); sin(x)]';
+
+for i = -5:.5:5
+    figure(1),clf;
+    hold on
+    T = [1 0; i 1];
+
+    plot(xy(:,1),xy(:,2),'bs', 'markerfacecolor', 'w');
+
+    newxy = xy * T;
+    plot(newxy(:,1),newxy(:,2),'bs', 'markerfacecolor', 'g');
+    axis([-1 1 -1 1] * max(abs([newxy(:); xy(:)])));
+    axis square;
+    pause(0.25);
+end
