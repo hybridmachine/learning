209945bbf1857b33e817691ccf349190e6db9c51
Brian Tabone <brian.tabone@gmail.com>
2020-12-19 10:25:08 -0600

    Checking in coding challenge inverse of diagonal matrices coding challenge

---
+++ matlab/udemy/Matrices/CodingChallengeInverseOfDiagonalMatrices.m
@@ -0,0 +1,13 @@
+% Create some diagonal matrices and take their inverses then think about
+% the observed pattern
+
+M1 = [2,0;0,2];
+inv(M1)
+
+M2 = [3,0;0,2];
+inv(M2)
+
+M3 = [3, 0, 0;0,6,0;0,0,9];
+inv(M3)
+
+% Diagonal matrices have their reciprocals as the inverse matrix
