94239752161370041123d04b53519033e3786c7a
Brian Tabone <brian.tabone@gmail.com>
2020-12-30 16:28:06 -0600

    Implementation of the Gram Schmidt process in code

---
+++ matlab/udemy/Matrices/CodingChallengeGramSchmidtProcedure.m
@@ -0,0 +1,42 @@
+% Implementation of the Gram-Schmidt 
+% Start with a square matrix
+% Check that Q'Q == Identity Matrix
+% Compare against built in function qr()
+
+% Gram Schmidt is to columns perpendicular to each other , start with left
+% most column as base (by convention)
+
+% Take left most column then decompose next column against it, find the
+% perpendicular component. Then decompose the next column against the
+% decomposed column, find perpendicular component and then take that and
+% find perpendicular component from first column and subtract it.
+
+m = 4;
+M1 = randi(100,m,m);
+MO = zeros(m,m);
+% Find orthoginal is
+% 1) dot(v1,ref)/dot(ref,ref) * ref == parallel_component
+% 2) v1 - parallel_component
+
+
+MO(:,1) = M1(:,1); % first column of orth matrix is the starting matrix
+
+for ColIdx = 2:m
+    OrthIdx = ColIdx;
+    ColVec = M1(:,ColIdx);
+    MO(:,ColIdx) = ColVec;
+    while OrthIdx > 1 
+        RefVec = MO(:,(OrthIdx - 1));
+        ParallelComponent = (dot(ColVec,RefVec)/dot(RefVec,RefVec)) * RefVec;
+        MO(:,ColIdx) = MO(:,ColIdx) - ParallelComponent;
+        OrthIdx = OrthIdx - 1;
+    end
+end
+
+% Q is the orthoginal unit matrix
+for ColIdx = 1:m
+    UnitCol = MO(:,ColIdx);
+    MO(:,ColIdx) = UnitCol / sqrt(dot(UnitCol,UnitCol));
+end
+
+
