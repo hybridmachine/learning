767b60b057cea8698aa8f01c20982ff9eb29f308
Brian Tabone <btabone@luminexcorp.com>
2021-03-18 11:29:00 -0500

    Checking in example from machine learning class

---
+++ math/arrays.py
@@ -0,0 +1,11 @@
+# create array
+from numpy import array
+# create array
+l = [[1.0, 2.0, 3.0],[4.0, 1.5, 3.3]]
+a = array(l)
+# display array
+print(a)
+# display array shape
+print(a.shape)
+# display array data type
+print(a.dtype)
