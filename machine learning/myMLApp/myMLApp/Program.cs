7be6908c34377223691d26ba8a4c6218b4452718
Brian Tabone <brian.tabone@gmail.com>
2021-03-29 23:35:37 -0500

    checking in latest

---
+++ machine learning/myMLApp/myMLApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MyMLAppML.Model;
+
+namespace myMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+            Console.WriteLine("Enter test text: ");
+            input.SentimentText = Console.ReadLine();
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+        }
+    }
+}
