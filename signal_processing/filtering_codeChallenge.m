b86833512fc4a4cea6d0e9e61a3bd3bae0b805fc
Brian Tabone <btabone@luminexcorp.com>
2021-05-22 18:27:47 -0500

    Implemented low and high pass filters, next up will be notch filter

---
+++ signal_processing/filtering_codeChallenge.m
@@ -0,0 +1,83 @@
+% Coding challenge to implement filter on x to match Professors output in y
+
+% Load and plot source signal and target output to match
+load filtering_codeChallenge.mat;
+
+time = 1:length(x);
+npnts = length(time);
+
+subplot(2,1,1);
+plot(time,x,time,y);
+
+xPow = fft(x);
+yPow = fft(y);
+
+
+
+% Looking at the target result, it looks like three filters
+% 1) A low pass filter with a cutoff around 35 hz (maybe a little earlier)
+% 2) A high pass filter 5hz and above
+% 3) A notch filter knocking out 17 to 24 hz
+
+
+% First the low pass filter (code borrowed from sigprocMXC_lowpass.m)
+
+fcutoff = 30;
+transw  = .25;
+order   = round( 19*fs/fcutoff );
+
+shape   = [ 1 1 0 0 ];
+frex    = [ 0 fcutoff fcutoff+fcutoff*transw fs/2 ] / (fs/2);
+
+% filter kernel
+filtkern = firls(order,frex,shape);
+
+% its power spectrum
+filtkernX = abs(fft(filtkern,npnts)).^2;
+
+figure(2), clf
+subplot(321)
+plot((-order/2:order/2)/fs,filtkern,'k','linew',3)
+xlabel('Time (s)')
+title('Filter kernel')
+
+subplot(322), hold on
+plot(frex*fs/2,shape,'r','linew',1)
+plot(hz,filtkernX(1:length(hz)),'k','linew',2)
+set(gca,'xlim',[0 60])
+xlabel('Frequency (Hz)'), ylabel('Gain')
+title('Filter kernel spectrum')
+
+xFilt = filtfilt(filtkern,1,x);
+xFiltPow = fft(xFilt);
+
+
+
+% high pass 5hz and up
+
+fcutoff = 5;
+order   = round( 15*fs/fcutoff );
+
+highPassKern = fir1(order,5/(fs/2),'high'); 
+highPassKernX = abs(fft(highPassKern,npnts)).^2;
+
+figure(3), clf
+subplot(321)
+plot((-order/2:order/2)/fs,highPassKern,'k','linew',3)
+xlabel('Time (s)')
+title('Filter kernel')
+
+subplot(322), hold on
+plot(hz,highPassKernX(1:length(hz)),'k','linew',2)
+set(gca,'xlim',[0 60])
+xlabel('Frequency (Hz)'), ylabel('Gain')
+title('Filter kernel spectrum')
+
+xFiltHigh = filtfilt(highPassKern,1,xFilt);
+xFiltHighPow = fft(xFiltHigh);
+
+figure(1);
+subplot(2,1,2);
+hz = linspace(0,fs,length(time));
+plot(hz,abs(xPow),hz,abs(yPow),hz,abs(xFiltHighPow));
+xlim([0 fs/2]);
