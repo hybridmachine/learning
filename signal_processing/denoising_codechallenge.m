eae0c307a678db7fee2abd0f3538be2f27609e9d
Brian Tabone <btabone@luminexcorp.com>
2021-06-13 17:26:06 -0500

    Checking in latest on wavelet_codeChallenge and added updates to denoising

--- signal_processing/denoising_codechallenge.m
+++ signal_processing/denoising_codechallenge.m
@@ -1,18 +1,25 @@
 % Denoise the signal code challenge
 load denoising_codeChallenge.mat
 
+plotRows = 5;
+plotCols = 1;
+plotIdx = 0;
+
 figure(1),clf;
-subplot(6,1,1);
+plotIdx = plotIdx + 1;
+subplot(plotRows,plotCols,plotIdx);
 plot(origSignal);
 title('Original Signal');
 n = length(origSignal);
 
-subplot(6,1,2);
-hist(origSignal,40);
+plotIdx = plotIdx + 1;
+subplot(plotRows,plotCols,plotIdx);
+histogram(origSignal,40);
 title('Histogram of values in signal');
-% Step 1 apply median filter for values above 5 (as visually noted in histogram above)
+
+% Step 1 apply median filter for values above 4 (as visually noted in histogram above)
 % visual-picked threshold
-threshold = 5;
+threshold = 4;
 
 
 % find data values above the threshold
@@ -33,50 +40,33 @@ for ti=1:length(suprathresh)
     filtsig(suprathresh(ti)) = median(origSignal(lowbnd:uppbnd));
 end
 
-meanFiltered = zeros(size(filtsig));
-
-% implement the running mean filter
-k = 100; % filter window is actually k*2+1
-for i=k+1:n-k-1
-    % each point is the average of k surrounding points
-    meanFiltered(i) = mean(filtsig(i-k:i+k));
-end
-
-% Gaussian filter
-% full-width half-maximum: the key Gaussian parameter
-fwhm = 65; % in au
-
-% normalized time vector in ms
-k = 45;
-gtime = (-k:k);
-
-% create Gaussian window
-gauswin = exp( -(4*log(2)*gtime.^2) / fwhm^2 );
-
-% initialize filtered signal vector
-filtsigG = filtsig;
-
-% implement the running mean filter
-for i=k+1:n-k-1
-    % each point is the weighted average of k surrounding points
-    filtsigG(i) = sum( filtsig(i-k:i+k).*gauswin );
-end
-
-subplot(6,1,3);
+plotIdx = plotIdx + 1;
+subplot(plotRows,plotCols,plotIdx);
 plot(filtsig);
 str_title = sprintf('Median filtered signal with cutoff at %d and window at %d',threshold,k);
 title(str_title);
 
-subplot(6,1,4);
+meanFiltered = zeros(size(filtsig));
+
+% implement the running mean filter. Updated from example online where
+% lower and upper bound was set to avoid clipping at end of signal
+k = 150; % filter window is actually k*2+1
+for i=1:n
+    lower_bound = max(1,i-k);
+    upper_bound = min(n,i+k);
+    meanFiltered(i) = mean(filtsig(lower_bound:upper_bound));
+    % each point is the average of k surrounding points
+end
+
+plotIdx = plotIdx + 1;
+subplot(plotRows,plotCols,plotIdx);
 plot(meanFiltered);
-str_title = sprintf('Mean filtered signal with window %d',k);
+str_title = sprintf('Moving mean filtered signal with window %d',k);
 title(str_title);
+axis([0 4000 -1 1]);
 
-subplot(6,1,5);
-plot(filtsigG);
-str_title = sprintf('Gaussian filtered signal with full width half maximum of %d',fwhm);
-title(str_title);
-
-subplot(6,1,6);
+plotIdx = plotIdx + 1;
+subplot(plotRows,plotCols,plotIdx);
 plot(cleanedSignal);
-title('Cleaned signal example for reference');
+title('Professor''s Cleaned signal example for reference');
+axis([0 4000 -1 1]);
--- signal_processing/wavelet_codeChallenge.m
+++ signal_processing/wavelet_codeChallenge.m
@@ -2,7 +2,12 @@
 % a FIR filter then a Morlet Wavelet
 load wavelet_codeChallenge.mat
 
-figure(1);
+% Setup plotting figures
+figFilterKernels = figure(3);
+set(figFilterKernels,'name','Filter Kernels');
+clf(figFilterKernels);
+
+figSignals = figure(1);
 numpts = length(signal);
 subplot(2,1,1);
 plot(signal);
@@ -18,15 +23,16 @@ xlabel('hz');
 title('Frequency Amplitude');
 
 % SignalFIR & signaMW
-figure(2); hold on;
+figFiltered = figure(2); 
+hold on;
 numpts = length(signalFIR);
-subplot(2,1,1);
+subplot(4,1,1);
 plot((1:numpts),signalFIR,(1:numpts),signalMW);
 xlim([0 numpts]);
-title('Filtered Signal');
+title('Reference Filtered Signal');
 legend('FIR','MW');
 
-subplot(2,1,2);
+subplot(4,1,3);
 signalX = abs(fft(signalFIR,numpts));
 signalMWX = abs(fft(signalMW,numpts));
 hz = linspace(0,srate,numpts);
@@ -34,3 +40,60 @@ plot(hz,signalX,hz,signalMWX);
 xlim([0 30]);
 xlabel('hz');
 title('Filtered Frequency Amplitude');
+
+% First implement a FIR filter to match
+% High pass first
+fcutoff = 8;
+order   = round( 15*srate/fcutoff );
+% 
+highPassKern = fir1(order,fcutoff/(srate/2),'high'); 
+highPassKernX = abs(fft(highPassKern,numpts)).^2;
+% 
+figure(figFilterKernels);
+subplot(321)
+plot(highPassKern,'k','linew',3)
+xlabel('Time (s)')
+title('High pass filter kernel')
+
+subplot(322), hold on
+plot(hz,highPassKernX(1:length(hz)),'k','linew',2)
+set(gca,'xlim',[0 60])
+xlabel('Frequency (Hz)'), ylabel('Gain')
+title('High pass filter kernel spectrum')
+
+sigFiltHighPass = filtfilt(highPassKern,1,signal);
+sigFiltHightPassX = fft(sigFiltHighPass);
+
+% Low pass next
+fcutoff = 17;
+order   = round( 15*srate/fcutoff );
+
+lowPassKern = fir1(order,fcutoff/(srate/2),'low'); 
+lowPassKernX = abs(fft(lowPassKern,numpts)).^2;
+
+figure(figFilterKernels);
+subplot(323)
+plot(lowPassKern,'k','linew',3)
+xlabel('Time (s)')
+title('Low pass filter kernel')
+
+subplot(324), hold on
+plot(hz,lowPassKernX(1:length(hz)),'k','linew',2)
+set(gca,'xlim',[0 60])
+xlabel('Frequency (Hz)'), ylabel('Gain')
+title('Low pass filter kernel spectrum')
+
+sigFiltLowPass = filtfilt(lowPassKern,1,sigFiltHighPass);
+sigFiltLowPassX = abs(fft(sigFiltLowPass));
+
+figure(figFiltered)
+
+subplot(4,1,2);
+plot((1:numpts),sigFiltLowPass);
+xlim([0 numpts]);
+title('Calculated Filtered Signal');
+
+subplot(4,1,4);
+plot(hz,sigFiltLowPassX);
+xlim([0 30]);
+title('Calculated Filter Kernel');
