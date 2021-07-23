971a62c85fa0c9e2fb3146fbab3b087afa420d3b
Brian Tabone <btabone@luminexcorp.com>
2021-07-23 12:13:07 -0500

    Added audio filter app and updated the base script

--- signal_processing/voice_memo_signal_processing.m
+++ signal_processing/voice_memo_signal_processing.m
@@ -6,22 +6,25 @@ timeLen = length(y) / Fs;
 timeVec = 0:1/Fs:timeLen;
 timeVec = timeVec(1:length(timeVec)-1);
 
-timeSeriesPlot = figure(1);
-timeSeriesPlot.Name = 'Time Series Plot';
+timeSeriesAndFrequencyPlot = figure(1);
+timeSeriesAndFrequencyPlot.Name = 'Time Series And Frequency Plot';
 clf;
+subplot(2,1,1);
 plot(timeVec, y);
 xlabel('Time');
 ylabel('Amplitude');
 title('Voice Memo Sample');
+xlim([0 timeLen]);
 
 % soundsc(y,Fs);
 
 hz = (0:Fs);
 freqData = fft(y,length(hz));
-frequencyPlotCutoff = 5000; % hz to cut plotting off at, only interested in voice range
+frequencyPlotCutoff = 3600; % hz to cut plotting off at, only interested in voice range
 
-frequencyPlot = figure(2);
-frequencyPlot.Name = 'Frequency Plot';
-clf;
+subplot(2,1,2);
 plot(hz,abs(freqData));
 xlim([0 frequencyPlotCutoff]);
+xlabel('Hz');
+ylabel('Amplitude');
+title('Voice Memo Frequency Amplitudes');
