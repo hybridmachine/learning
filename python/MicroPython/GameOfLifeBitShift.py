3107c53607be120ae8dbac908dc019d27d18c63a
Brian Tabone <btabone@luminexcorp.com>
2023-05-18 18:41:20 -0500

    Update to latest, starting to render on CosmicUnicorn

--- python/MicroPython/GameOfLifeBitShift.py
+++ python/MicroPython/GameOfLifeBitShift.py
@@ -1,6 +1,27 @@
-###########
-# Implementation of John Conway's game of life using bit shifting in Python, targeting MicroPython
-# The intention is to use this to drive a Pimoroni Cosmic LED grid
+import array
+import time
+import machine
+from cosmic import CosmicUnicorn
+from picographics import PicoGraphics, DISPLAY_COSMIC_UNICORN as DISPLAY
+
+graphics = None
+palette = None
+
+# create cosmic object and graphics surface for drawing
+cosmic = CosmicUnicorn()
+graphics = PicoGraphics(DISPLAY)
+
+def init():
+    # a palette of five firey colours (white, yellow, orange, red, smoke)
+    global palette
+    palette = [
+        graphics.create_pen(0, 0, 0),
+        graphics.create_pen(20, 20, 20),
+        graphics.create_pen(180, 30, 0),
+        graphics.create_pen(220, 160, 0),
+        graphics.create_pen(255, 255, 180)
+    ]
+    
 def GoL_FindNextBitState(currentBitState, neighborCount):
     # John Conway's Game Of Life
     # From Wikipedia
@@ -18,6 +39,51 @@ def GoL_FindNextBitState(currentBitState, neighborCount):
         else:
             return 0
 
+
+def GoL_FindNeighborCount(TwoDBitContext):
+    nextGenRowMask = 0b111
+
+    ngRowTop = currentTop & nextGenRowMask
+
+    ngRowMid = currentMid & nextGenRowMask
+    bitUnderConsideration = (ngRowMid >> 1) & 1
+    ngRowMid = ngRowMid & 0b101
+
+    ngRowBottom = currentBottom & nextGenRowMask
+  
+
+def GoL_InitR_Pentomino(playbrd):
+    playbrd[15] = 0b00000000000000011000000000000000
+    playbrd[16] = 0b00000000000000110000000000000000
+    playbrd[17] = 0b00000000000000010000000000000000
+
+
+def GoL_DisplayOnCosmicUnicorn(playbrd):
+    # render the heat values to the graphics buffer
+    for y in range(CosmicUnicorn.HEIGHT):
+        for x in range(CosmicUnicorn.WIDTH):
+            bitVal = (playbrd[y] >> x) & 1
+            
+            if bitVal == 1:
+                graphics.set_pen(palette[3])
+                #print ("bitVal is {0},{1} -> {2}".format(x, y, bitVal))
+            else:
+                graphics.set_pen(palette[0])
+            graphics.pixel((CosmicUnicorn.WIDTH - x), y)
+            
+    cosmic.set_brightness(0.75)
+    cosmic.update(graphics)
+     
+init()
+
+currentIdx = 0
+futureIdx = 1
+
+playBoard = [0,0]
+playBoard[0] = array.array("I", 0 for x in range(0, 32))
+playBoard[1] = array.array("I", 0 for x in range(0, 32))
+
+GoL_InitR_Pentomino(playBoard[currentIdx])
 
 currentTop =    0b000000
 currentMid =    0b001111
@@ -46,4 +112,10 @@ for idx in range(0,3,1):
 print("Neighborcount {0}".format(neighborCount))
 print("bitUnderConsideration {0}".format(bitUnderConsideration))
 print("bitFuture {0}".format(GoL_FindNextBitState(bitUnderConsideration, neighborCount)))
+print("playboardSize {0}".format(len(playBoard[currentIdx])))
+print("playboard {0}".format(playBoard[currentIdx]))
+
+while True:
+    GoL_DisplayOnCosmicUnicorn(playBoard[currentIdx])
+    time.sleep(2)
 
