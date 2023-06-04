c43980d1f2576fbf43c7740d3e9217afc8057247
Brian Tabone <btabone@luminexcorp.com>
2023-06-04 14:32:17 -0500

    Adding test harness and updates to distribute clock

--- FPGA/Thermostat/Thermostat.srcs/sources_1/new/Thermostat.vhd
+++ FPGA/Thermostat/Thermostat.srcs/sources_1/new/Thermostat.vhd
@@ -39,40 +39,87 @@ entity Thermostat is
         acControlSwitch : in STD_LOGIC;
         heatControlSwitch : in STD_LOGIC;
         acPowerControl : out STD_LOGIC;
-        heatPowerControl : out STD_LOGIC );
+        heatPowerControl : out STD_LOGIC;
+        clk : in STD_LOGIC );
 end Thermostat;
 
 architecture Behavioral of Thermostat is
+
+    --Inbound signals
+    signal desiredTemp_reg : STD_LOGIC_VECTOR(6 downto 0);
+    signal currentTemp_reg : STD_LOGIC_VECTOR(6 downto 0); 
+    signal displaySel_reg : STD_LOGIC;
+    signal acControlSwitch_reg : STD_LOGIC; 
+    signal heatControlSwitch_reg : STD_LOGIC;
+    
+    --Outbound signals
+    signal acPowerControl_reg : STD_LOGIC;
+    signal heatPowerControl_reg : STD_LOGIC;
+    signal displayedTemp_reg : STD_LOGIC_VECTOR(6 downto 0);
+    
+    
 begin
 
-    process(displaySel, desiredTemp, currentTemp)
+    -- Input flipflops
+    process(clk, desiredTemp, currentTemp, displaySel, acControlSwitch, heatControlSwitch)
     begin
-        if displaySel = '0' then
-            displayedTemp <= desiredTemp;
+        if clk'event and clk = '1' then
+            desiredTemp_reg <= desiredTemp;
+            currentTemp_reg <= currentTemp;
+            displaySel_reg <= displaySel;
+            acControlSwitch_reg <= acControlSwitch;
+            heatControlSwitch_reg <= heatControlSwitch;
         else
-            displayedTemp <= currentTemp;
+            desiredTemp_reg <= desiredTemp_reg;
+            currentTemp_reg <= currentTemp_reg;
+            displaySel_reg <= displaySel_reg;
+            acControlSwitch_reg <= acControlSwitch_reg;
+            heatControlSwitch_reg <= heatControlSwitch_reg;
+        end if;
+    end process;
+
+    -- Output flipflops
+    process(clk, acPowerControl_reg, heatPowerControl_reg, displayedTemp_reg)
+    begin
+        if clk'event and clk = '1' then
+            displayedTemp <= displayedTemp_reg;
+            acPowerControl <= acPowerControl_reg;
+            heatPowerControl <= heatPowerControl_reg;
+        --else
+        --    acPowerControl_reg <= acPowerControl_reg;
+        --    heatPowerControl_reg <= heatPowerControl_reg;
+        --    displayedTemp_reg <= displayedTemp_reg;
         end if;
     end process;
     
-    process(acControlSwitch, heatControlSwitch, currentTemp, desiredTemp)
-    begin
+    process(displaySel_reg, desiredTemp_reg, currentTemp_reg)
+    begin 
+        if displaySel_reg = '0' then
+            displayedTemp_reg <= desiredTemp_reg;
+        else
+            displayedTemp_reg <= currentTemp_reg;
+        end if;
+    end process;
+    
+    process(acControlSwitch_reg, heatControlSwitch_reg, currentTemp_reg, desiredTemp_reg)
+    begin       
         -- Default to off , set power state based on switch and temp state
         -- This way if user switches from cool to heat or vice versa, we'll turn off the opposite
         -- mode by default
-        acPowerControl <= '0';
-        heatPowerControl <= '0';
+        acPowerControl_reg <= '0';
+        heatPowerControl_reg <= '0';
         
-        if acControlSwitch = '1' then
-            if unsigned(currentTemp) > unsigned(desiredTemp) then
-                acPowerControl <= '1';
+        if acControlSwitch_reg = '1' then
+            if unsigned(currentTemp_reg) > unsigned(desiredTemp_reg) then
+                acPowerControl_reg <= '1';
             else
-                acPowerControl <= '0';
+                acPowerControl_reg <= '0';
             end if;
-        elsif heatControlSwitch = '1' then
-            if unsigned(currentTemp) < unsigned(desiredTemp) then
-                heatPowerControl <= '1';
+        elsif heatControlSwitch_reg = '1' then
+            if unsigned(currentTemp_reg) < unsigned(desiredTemp_reg) then
+                heatPowerControl_reg <= '1';
             else
-                heatPowerControl <= '0';
+                heatPowerControl_reg <= '0';
             end if;
         end if; 
     end process;
