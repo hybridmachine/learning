dcb447a6ee6ac02da1101e2c22c589c3d28ac183
Brian Tabone <brian.tabone@gmail.com>
2025-03-13 21:55:31 -0500

    Added breakout of each component of the floating point number

--- c_programming/explore_floating_point.c
+++ c_programming/explore_floating_point.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <math.h>
 
 #define EXPONENT_ALL_ONES 255 << 23
+
+const int single_precision_bias = 127;
+
 // Function to print bits of an unsigned integer in 32 bit float format
 void print_bits(uint32_t num) {
 	for (int i = 31; i >= 0; i--) {
@@ -18,12 +22,43 @@ void print_bits(uint32_t num) {
 void print_float_info(char *note, uint32_t float_mem)
 {
 	printf("++++++++++++++++++++++++++++\n");
+
+	
+	int sign = (float_mem >> 31) & 1;
+	int exponent = ((float_mem >> 23) & 0xFF) - single_precision_bias;
+	int mantissa_bits = float_mem & 0x7FFFFF;
+	
+	printf("Sign: %d\n", sign);
+	printf("Exponent: %d\n", exponent);
+	printf("Fraction: ");
+	char fracStr[512];
+	memset(&fracStr, 0, sizeof(fracStr));
+	int first = 0;
+
+	for (int idx = 1; idx < 24; idx++)
+	{
+		int shift = 23 - idx;
+		int bit = (mantissa_bits >> shift) & 0x01;
+		int divisor = pow(2,idx);
+
+		if (bit == 1)
+		{
+			if (first > 0 && divisor > 2)
+			{
+				sprintf(fracStr, " + ");
+			}
+			first++;
+			sprintf(fracStr, "1/%d", divisor);
+		}
+	}
+	printf("%s\n\n", fracStr);
+	printf("Float value is %d * (1/%d) * (1 + %s)\n\n", sign ? -1 : 1, (int)pow(2,abs(exponent)), fracStr);
 	printf("%s\n\n", note);
 	float float_val;
 	memcpy(&float_val, &float_mem, sizeof(float));
 	printf("Raw mem val is 0x%x\n", (uint32_t)float_mem);
 	print_bits(float_mem);
-	printf("\nFloat val is %g\n", float_val);
+	printf("\nCalculated float val is %g\n", float_val);
 	printf("-----------------------------\n\n");
 }
 
@@ -64,6 +99,11 @@ int main(int argc, char **argv) {
 
 	float_val = 0.15625;
 	memcpy(&float_mem, &float_val, sizeof(float));
-	print_float_info("\tfloat_val = 0.5;\n\tmemcpy(&float_mem, &float_val, sizeof(float));", float_mem);
+	print_float_info("\tfloat_val = 0.15625;", float_mem);
+
+	float_val = -0.15625;
+	memcpy(&float_mem, &float_val, sizeof(float));
+	print_float_info("\tfloat_val = -0.15625;", float_mem);
+	
 	return 0;	
 }
